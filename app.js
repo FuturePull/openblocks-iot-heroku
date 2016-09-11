f6d0fbd77ab45eb2ec1319fb2196f538f5d806b0
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-12 07:09:06 +0900

    minimum endpoint function

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-'use strict';
+void function () {
+	'use strict';
 
-const http = require('http');
+	const http = require('http');
+	const list = ['first'];
+	const releaseDate = '2016-09-12 07:00 JST';
 
-http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-	res.end('<p>hello world 2016-09-11 10:30 JST</p>');
-}).listen(process.env.PORT || 3000);
+	http.createServer((req, res) => {
+		if (req.url.startsWith('/endpoint')) {
+			if (req.method === 'POST') {
+				let str = '';
+				req.on('data', (data) => {
+					str += data;
+				});
+				req.on('end', () => {
+					list.append(str);
+					if (list.length > 100) list.shift();
+					res.end('');
+				});
+			}
+			else {
+				res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+				res.write('<pre>\r\n');
+				list.forEach(s => res.write(s + '\r\n'));
+				res.write('</pre>\r\n');
+				res.end('<p>hello world ' + releaseDate + '</p>\r\n');
+			}
+			return;
+		}
+		res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+		res.end('<p>hello world ' + releaseDate + '</p>');
+	}).listen(process.env.PORT || 3000);
+
+} ();
