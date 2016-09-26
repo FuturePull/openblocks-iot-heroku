345e3ee16dca30a4eec996d2db0e56c50973667a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-26 15:41:19 +0900

    prestart npm install

--- app.cmd
+++ app.cmd
@@ -1,5 +1,7 @@
 @rem set DEBUG=app:*,express:*
+node tools/prestart
 set APP_URLS=http://localhost:3000
 set DEBUG=app:*,express:application,express:router
 start npm start
 start http://localhost:3000
+pause
