ead94d2380c9d548e68185b6e2d13b0f198c3036
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-09-29 07:25:37 +0900

    socket.io

--- app.js
+++ app.js
@@ -150,6 +150,17 @@
 		debug('Listening on port ' + server.address().port);
 	}
 
+
+	// socket.io
+	const io = require('socket.io')(server);
+
+	io.on('connection', function (socket) {
+		socket.emit('news', { hello: 'world' });
+		socket.on('my other event', function (data) {
+			console.log(data);
+		});
+	});
+
 	// APP_URLS polling
 	(process.env.APP_URLS || '')
 		.split(';')
